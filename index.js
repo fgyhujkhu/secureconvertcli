c09626e39172368a7d7eb560ddb9731ea1a2ff71
fgyhujkhu <jsjcungsehyun31@hotmail.com>
2024-05-09 14:43:48 +0700

    update sdk task

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
-function deserialize(data) {
-  if (!data.length) return null;
-  const root = new TreeNode(data.shift());
-  const queue = [root];
-  while (queue.length) {
-    const node = queue.shift();
-    const leftVal = data.shift();
-    if (leftVal !== undefined) {
-      node.left = new TreeNode(leftVal);
-      queue.push(node.left);
-    }
-    const rightVal = data.shift();
-    if (rightVal !== undefined) {
-      node.right = new TreeNode(rightVal);
-      queue.push(node.right);
-    }
-  }
-  return root;
+function fibonacci(n) {
+  if (n <= 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
